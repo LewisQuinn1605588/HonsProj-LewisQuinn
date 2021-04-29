fc5b370bd38e663214368de4fde68e9720bff6da
LewisQuinn1605588 <l.quinn3@rgu.ac.uk>
2021-04-29 02:58:37 +0100

    14.3

--- script.js
+++ script.js
@@ -40,3 +40,11 @@ function openForm() {
 function closeForm() {
   document.getElementById("myForm").style.display = "none";
 }
+
+function openForm2() {
+  document.getElementById("myForm2").style.display = "block";
+}
+
+function closeForm() {
+  document.getElementById("myForm2").style.display = "none";
+}
